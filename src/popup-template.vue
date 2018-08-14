3359ffe486f572ed3a99250d9f5c6b1580652432
mengdu <1752295326@qq.com>
2018-08-14 23:25:22 +0800

    update popup.js

---
+++ src/popup-template.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="m-popup">
+    <transition name="popup-fade">
+    <div class="m-popup-wrapper" v-if="show"></div>
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      show: false
+    }
+  }
+}
+</script>
--- src/popup.js
+++ src/popup.js
@@ -1,49 +1,49 @@
 import Vue from 'vue'
-
+import PopupTemplate from './popup-template'
 const instances = []
 let count = 1
 let Popup = null
 
-const PopupTemplate = {
-  data () {
-    return {
-      show: false
-    }
-  },
-  render (h) {
-    console.log(this)
-    if (!this.show) {
-      return null
-    }
-    const children = [h('div', {'class': 'm-dialog__wrapper'})]
-    return h('transition', {name: 'dialog-fade'}, children)
-  }
-}
+// const PopupTemplate = {
+//   data () {
+//     return {
+//       show: false
+//     }
+//   },
+//   render (h) {
+//     console.log(this)
+//     if (!this.show) {
+//       return null
+//     }
+//     const children = [h('div', {'class': 'm-dialog__wrapper'})]
+//     return h('transition', {name: 'dialog-fade'}, children)
+//   }
+// }
 
 const Mask = Vue.extend(PopupTemplate)
 
 export default {
   created () {
-    // if (!Container) {
-    //   Container = document.createElement('div')
-    //   Container.className = 'm-popup-container'
-    //   document.body.appendChild(Container)
-    // }
     if (!Popup) {
       Popup = new Mask({
         el: document.createElement('div')
       })
       document.body.appendChild(Popup.$el)
-      Popup.show = true
     }
 
     this.id = ++count
     instances.push(this)
+
+    if (this.show) {
+      Popup.show = true
+    }
   },
   mounted () {
-    console.log(this.$el)
+    if (Popup && this.appendToBody) {
+      Popup.$el.appendChild(this.$el)
+    }
     document.body.onclick = function () {
-      console.log(Popup)
+      console.log(instances)
       Popup.show = !Popup.show
     }
   }
