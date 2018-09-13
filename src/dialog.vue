be144c8e6e0e5b8f424915ad34ade6c01a74e4f6
v_yguiliu <v_yguiliu@tencent.com>
2018-09-13 17:50:19 +0800

    更新dialog

--- src/dialog.vue
+++ src/dialog.vue
@@ -21,11 +21,34 @@
 </template>
 <script>
 import popup from './popup'
-import props from './props'
+
 export default {
   name: 'MDialog',
   mixins: [popup],
-  props,
+  props: {
+    width: String,
+    autoWidth: Boolean,
+    top: String,
+    show: Boolean,
+    title: String,
+    noHead: Boolean,
+    appendToBody: Boolean,
+    closeOnClickModal: {
+      type: Boolean,
+      default: true
+    },
+    closeOnPressEscape: {
+      type: Boolean,
+      default: true
+    },
+    showClose: {
+      type: Boolean,
+      default: true
+    },
+    beforeClose: Function,
+    isMiddle: Boolean,
+    marginTop: String
+  },
   data () {
     return {
       isCurrent: false,
