83fc9184ea531d6bcd81baacd7c048e5f2a18602
v_yguiliu <v_yguiliu@tencent.com>
2019-04-04 16:54:39 +0800

    feat: 添加 use 配置默认值

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ Dialog.install = function (Vue, options = {}) {
   Vue.component(options.dialogName || Dialog.name, Dialog)
   Vue.prototype['$' + (options.alertName || 'alert')] = Alert
   Vue.prototype['$' + (options.confirmName || 'confirm')] = Confirm
+
+  if (typeof options.alertOptions === 'object') Alert.config(options.alertOptions)
+  if (typeof options.confirmOptions === 'object') Confirm.config(options.confirmOptions)
 }
 
 export {
