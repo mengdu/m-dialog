f1e7b2c6bfd72ecada1124cf7f5f1fafa188d29e
v_yguiliu <v_yguiliu@tencent.com>
2019-04-04 10:43:41 +0800

    更新例子

--- docs/App.vue
+++ docs/App.vue
@@ -11,13 +11,13 @@
       </div>
     </header>
     <main class="doc-block">
-      <readme />
-      <!-- <m-dialog
+      <m-dialog
         title="This is info message !"
         :show.sync="show2"
         auto-width
         >
-        <span>This is content.</span>
+        <span>This is content.</span>&nbsp;
+        <m-button @click="show3 = !show3" size="mini" type="info">Preview</m-button>
         </m-dialog>
       <m-dialog 
         :show.sync="show"
@@ -36,24 +36,46 @@
         <span>这里是正文内容...</span>
         <div slot="footer">
           <div style="float: right">
-            <m-button plain @click="show = false">取消</m-button>
+            <m-button plain @click="handleConfirm1">取消</m-button>
             <m-button type="info" @click="show2 = true">确定</m-button>
           </div>
           
         </div>
       </m-dialog>
       
-      <m-button @click="show = !show" type="info">modal</m-button>
-      <m-button @click="show2 = !show2" type="info">modal2</m-button>
-      <m-button @click="handleAlert1">alert</m-button>
-      <m-button @click="handleConfirm1">confirm</m-button>
+      <m-dialog
+        title="预览"
+        auto-width
+        no-head
+        :show.sync="show3"
+        >
+          <img src="https://avatars0.githubusercontent.com/u/11366654?s=460&v=4" alt=""/>
+        </m-dialog>
+
+      <m-dialog
+        title="Iframe"
+        :show.sync="show4"
+        auto-width
+        >
+         <iframe src="https://www.lanyueos.com/" frameborder="0" style="width: 1000px;height: 600px;"></iframe>
+        </m-dialog>
+
+      <m-button @click="show = !show" size="small" type="info">modal</m-button>
+      <m-button @click="show2 = !show2" size="small" type="info">modal2</m-button>
+      <m-button @click="show3 = !show3" size="small" type="info">modal3</m-button>
+      <m-button @click="show4 = !show4" size="small" type="info">Iframe</m-button>
+      <m-button @click="handleAlert1" size="small" type="danger">alert</m-button>
+      <m-button @click="handleConfirm1" size="small" type="success">confirm</m-button>
       <p>
         <label><input type="checkbox" v-model="appendToBody">append-to-body</label>
         <label><input type="checkbox" v-model="noHead">no-head </label>
         <label><input type="checkbox" v-model="isMiddle">is-middle</label>
         <label><input type="checkbox" v-model="closeOnClickModal">close-on-click-modal </label>
         <label><input type="checkbox" v-model="closeOnPressEscape">close-on-press-escape </label>
-      </p> -->
+      </p>
+    </main>
+    <main class="doc-block">
+      <readme />
     </main>
     <main class="doc-block">
       <doc />
@@ -82,6 +104,8 @@ export default {
       pkg,
       show: false,
       show2: false,
+      show3: false,
+      show4: false,
       appendToBody: false,
       noHead: false,
       closeOnClickModal: true,
@@ -104,20 +128,26 @@ export default {
       this.$alert('Alert 消息框：<strong>加粗显示</strong>', {
         hasCancelButton: true,
         supperHTMLString: true,
+        iconType: 'question',
         callback (type) {
           console.log('alert cb')
         }
       })
     },
     handleConfirm1 () {
-      this.$confirm('你确定要删除么？', {
+      this.$confirm('你确定要退出么？', {
         title: '警告',
         closeOnPressEscape: true,
         confirmButtonType: 'danger',
-        typeIcon: 'danger',
+        confirmButtonClassName: 'm-button m-button-info',
+        iconType: 'error',
       }).then(() => {
         console.log('confirm')
         // alert('确认了操作')
+
+        setTimeout(() => {
+          this.show = false
+        }, 500)
       }).catch(() => {
         console.log('cancel')
         // alert('取消了操作')
