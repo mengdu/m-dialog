9c24dce5cfe7653553eb2399da56c43ce969629c
mengdu <1752295326@qq.com>
2019-04-04 21:25:52 +0800

    merge

--- src/icon/icon.js
+++ src/icon/icon.js
@@ -48,11 +48,8 @@ const Icon = {
 
 Icon.add = addIcon
 
-<<<<<<< HEAD
-=======
 Icon.has = function (name) {
   return !!icons[name]
 }
 
->>>>>>> dev
 export default Icon
--- src/icon/index.js
+++ src/icon/index.js
@@ -4,22 +4,14 @@ import SuccessIcon from './svgs/success'
 import ErrorIcon from './svgs/error'
 import WarningIcon from './svgs/warning'
 import LoadingIcon from './svgs/loading'
-<<<<<<< HEAD
-=======
 import QuestionIcon from './svgs/question'
->>>>>>> dev
 
 Icon.add(InfoIcon.name, InfoIcon)
 Icon.add(SuccessIcon.name, SuccessIcon)
 Icon.add(ErrorIcon.name, ErrorIcon)
-<<<<<<< HEAD
-Icon.add(WarningIcon.name, WarningIcon)
-Icon.add(LoadingIcon.name, LoadingIcon)
-=======
 Icon.add('danger', ErrorIcon)
 Icon.add(WarningIcon.name, WarningIcon)
 Icon.add(LoadingIcon.name, LoadingIcon)
 Icon.add(QuestionIcon.name, QuestionIcon)
->>>>>>> dev
 
 export default Icon
